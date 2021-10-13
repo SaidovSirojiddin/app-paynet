8b7cac5b5b0822c4a948790b00cf7e10abf34e5c
sirojiddinEcma <64889105+sirojiddinEcma@users.noreply.github.com>
2021-10-13 10:45:46 +0500

    Update UserRepository.java

--- src/main/java/ai/ecma/paynet/repository/UserRepository.java
+++ src/main/java/ai/ecma/paynet/repository/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<Client, Integer> {
+public interface ClientRepository extends JpaRepository<Client, Integer> {
 
     Optional<Client> findByUsername(String username);
 
